ccb0c0f558f99f320e43f25627b811eb10674d2d
Ali Shirmohammadi <ali.shirmohammadi1997@gmail.com>
2023-04-06 06:24:40 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -11,6 +11,9 @@
 
 // console.log("Now you're ready to go outside!");
 
+
+
+
 // const cold = false;
 
 // if (cold) {
@@ -19,14 +22,32 @@
 //   console.log("Short sleeves are fine.");
 // }
 
-const temperature = -4;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+
+
+// const temperature = -4;
+
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+
+
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// }
+
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 }
-
-console.log("Now you're ready to go outside!");
